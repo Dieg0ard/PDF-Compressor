17cca2892e6e0a212b9fd6e2dcc1742717621514
Dieg0ard <102417784+Dieg0ard@users.noreply.github.com>
2024-07-09 17:56:29 -0500

    Create images.py
    a

---
+++ PDF_MASTER/pdf_compressor/images.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import io
+
+def comprimirJPEG(datos):
+    img = Image.open(io.BytesIO(datos))
+    img_comprimida = io.BytesIO()
+    img.save(img_comprimida, optimize=True, format="JPEG", quality=50)
+    img_comprimida.seek(0)
+    return img_comprimida.getvalue()
+
+def comprimirPNG(datos):
+    img = Image.open(io.BytesIO(datos))
+    img_comprimida = io.BytesIO()
+    img = img.convert("P", palette=Image.ADAPTIVE, colors=100)  # Reducir colores
+    img.save(img_comprimida, format='PNG', optimize=True)
+    img_comprimida.seek(0)
+    return img_comprimida.getvalue()
+
+def comprimirTIFF(datos):
+    img = Image.open(io.BytesIO(datos))
+    img_comprimida = io.BytesIO()
+    img.save(img_comprimida, format='TIFF', compress_level=9)
+    img_comprimida.seek(0)
+    return img_comprimida.getvalue()
+
+# def comprimirJPEG2000(datos):
+#     img = imagecodecs.imread(io.BytesIO(datos))
+#     img_comprimida = io.BytesIO()
+#     imagecodecs.imwrite(img_comprimida, img, codec='jp2')
+#     img_comprimida.seek(0)
+#     return img_comprimida.getvalue()
